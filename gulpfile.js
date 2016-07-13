b509c4781598b390e93a0265d969f4495c978301
Peter Snider <snider.peter@gmail.com>
2016-07-12 18:25:34 -0700

    automated gulp test-end-to-end

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var   Server = require('karma').Server;
 var protractor = require("gulp-protractor").protractor;
 const del = require('del');
 const minimist = require('minimist');
-
+const child_process = require('child_process');
 
 var knownOptions = {
     string: ['env', 'hostname', 'port', 'mongo-path', 'node-inspector-port' ],
@@ -18,6 +18,88 @@ var knownOptions = {
 
 
 var options = minimist(process.argv.slice(2), knownOptions);
+
+
+
+// returns a Promise, resolving with the child process, or rejecting with an Error
+function startProcess(options) {
+    return new Promise(function(resolve, reject) {
+        // DEBUG: console.log(`starting ${options.cmd} ${options.args}`)
+        var child = child_process.spawn(options.cmd, options.args)
+        child.on('error', (err) => {
+            reject(err)
+        });
+        child.on('exit', (code, signal) => {
+            // DEBUG: console.log(`exit ${options.cmd} ${options.args}: code=${code} signal=${signal}`)
+        });
+        var delay = options.delay || 0
+        setTimeout(() => {
+            resolve({child})
+        }, delay)
+    })
+}
+
+
+// returns a Promise, resolving with an array of the child processes, or rejecting with an Error
+function startProcesses(processes) {
+    var promises = []
+    processes.forEach((process) => {
+        promises.push(startProcess(process))
+    })
+    return Promise.all(promises)
+}
+
+
+// returns a Promise, resolving with the child process
+function stopProcess(options) {
+    return new Promise(function(resolve, reject) {
+        // DEBUG: console.log(`stopping ${options.child.spawnargs}`)
+        child_process.exec(`pgrep -P ${options.child.pid}`, (error, stdout, stderr) => {
+            // DEBUG: console.log('child.kill(SIGINT)')
+            options.child.kill('SIGINT')
+            // DEBUG: console.log(`pgrep -P ${options.child.pid}  =>  ${stdout}`)
+            var childs_children = stdout.trim().split(/\s/)
+            childs_children.forEach((childs_child) => {
+                // DEBUG: console.log(`kill -KILL ${childs_child}`)
+                child_process.exec(`kill -KILL ${childs_child}`, (error, stdout, stderr) => {
+                    if (error) {
+                        reject(error)
+                    }
+                })
+            })
+            var delay = options.delay || 0
+            setTimeout(() => {
+                resolve()
+            }, delay)
+        })
+    })
+}
+
+
+// returns a Promise, resolving with an array of the child processes
+function stopProcesses(children) {
+    var promises = []
+    // DEBUG: console.log(`children=${children}`)
+    children.forEach((child) => {
+        promises.push(stopProcess(child))
+    })
+    return Promise.all(promises)
+}
+
+
+function runServers() {
+    var processes = [
+        // Cannot figure out how to programmatically shut down webdriver-manager
+        // It seems it hangs gulp (due to child processes, other resources)?
+        {cmd:'node_modules/gulp-protractor/node_modules/protractor/bin/webdriver-manager', args: ['start'], delay: 2000},
+        // {cmd:'/usr/bin/vim', args: ['vim.tmp'], delay: 1000},
+        // {cmd:'/usr/bin/vim', args: ['vim2.tmp'], delay: 1000}
+        {cmd:'/usr/local/bin/node', args: ['src/server/js/people-pin-service.js'], delay: 1000},
+        {cmd:'/usr/local/bin/node', args: ['src/server/js/people-app.js'], delay: 1000}
+    ]
+    return startProcesses(processes)
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // all
@@ -145,9 +227,16 @@ gulp.task('build-end-to-end-tests', function() {
 
 
 
-gulp.task('test-end-to-end', ['build-client', 'build-server', 'build-end-to-end-tests'], function() {
-    console.warn("WARNING: Assuming you have already run: webdriver-manager start")
-    console.warn("WARNING: Assuming you have already run: bin/start-servers.sh --log --save")
+
+var test_end_to_end_promises
+gulp.task('start-servers-for-end-to-end-tests', ['build-client', 'build-server', 'build-end-to-end-tests'], function() {
+    test_end_to_end_promises = runServers()
+    return test_end_to_end_promises
+});
+
+
+gulp.task('test-end-to-end', ['start-servers-for-end-to-end-tests'], function(done) {
+    // TODO: set NODE_ENV=tests
     var stream = gulp.src(["./test/*.js"])
         .pipe(protractor({
             configFile: "test/end-to-end/protractor.conf.js",
@@ -155,7 +244,11 @@ gulp.task('test-end-to-end', ['build-client', 'build-server', 'build-end-to-end-
         }))
         .on('error', function(e) { throw e })
     stream.on('end', function() {
-        console.warn("WARNING: You must manually stop the servers: bin/stop-servers.sh")
+        test_end_to_end_promises.then((children) => {
+            stopProcesses(children).then(() => {
+                // DEBUG: console.log('stopProcesses resolved')
+                done()
+            })
+        })
     });
-    return stream
 });
