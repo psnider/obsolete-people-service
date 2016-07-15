a2bbe7a466505527511521068e637b5c559f36eb
Peter Snider <snider.peter@gmail.com>
2016-07-15 14:11:59 -0700

    added test data

---
+++ components/server/test/data/mock-people.js
@@ -0,0 +1,196 @@
+System.register([], function(exports_1) {
+    var PEOPLE;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("PEOPLE", PEOPLE = [
+                {
+                    "name": {
+                        "given": "连杰",
+                        "family": "李"
+                    },
+                    "locale": "zh_SG",
+                    "time_zone": "Asia/Singapore",
+                    "contact_methods": [
+                        { "method": "email", "address": "Jet.Li@fakemail.com" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Marge",
+                        "family": "Simpson"
+                    },
+                    "locale": "en_US",
+                    "time_zone": "America/Chicago",
+                    "contact_methods": [
+                        { "method": "email", "address": "Marge.Simpson@fakemail.com" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Gilligan"
+                    },
+                    "locale": "en_US",
+                    "time_zone": "Pacific/Honolulu",
+                    "contact_methods": [
+                        { "method": "message in a bottle", "address": "pacific ocean" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "トトロ"
+                    },
+                    "locale": "jp_JP",
+                    "time_zone": "Asia/Tokyo"
+                },
+                {
+                    "name": {
+                        "given": "Sonmi~451"
+                    },
+                    "locale": "en_KR",
+                    "time_zone": "Asia/Seoul",
+                    "contact_methods": [
+                        { "method": "facebook", "address": "Sonmi~451" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Wile",
+                        "family": "Coyote"
+                    },
+                    "locale": "en_US",
+                    "time_zone": "America/Phoenix"
+                },
+                {
+                    "name": {
+                        "given": "John",
+                        "family": "Hammond"
+                    },
+                    "locale": "en_US",
+                    "time_zone": "America/Costa_Rica",
+                    "contact_methods": [
+                        { "method": "email", "address": "John.Hammond@fakemail.com" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Jay"
+                    },
+                    "locale": "en_US",
+                    "time_zone": "America/New_York",
+                    "contact_methods": [
+                        { "method": "facebook", "address": "meninblack" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Jax",
+                        "family": "Teller"
+                    },
+                    "locale": "en_US",
+                    "time_zone": "America/Los_Angeles",
+                    "contact_methods": [
+                        { "method": "phone", "address": "1 555 jax-tell" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Walter",
+                        "family": "White"
+                    },
+                    "locale": "en_US",
+                    "time_zone": "America/Phoenix",
+                    "contact_methods": [
+                        { "method": "phone", "address": "1 555 jax-tell" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Jimmy",
+                        "family": "McGill"
+                    },
+                    "locale": "en_US",
+                    "time_zone": "America/Phoenix",
+                    "contact_methods": [
+                        { "method": "phone", "address": "1 505 842-5662" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Tony",
+                        "family": "Soprano"
+                    },
+                    "locale": "en_US",
+                    "time_zone": "America/New_York",
+                    "contact_methods": [
+                        { "method": "phone", "address": "1 856-783-0800" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Wallace"
+                    },
+                    "locale": "en_UK",
+                    "time_zone": "Europe/London",
+                    "contact_methods": [
+                        { "method": "twitter", "address": "#WallaceandGromit" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Jacques",
+                        "family": "Clouseau"
+                    },
+                    "locale": "fr_FR",
+                    "time_zone": "Europe/Paris",
+                    "contact_methods": [
+                        { "method": "email", "address": "Jacques.Clouseau@fakemail.com" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Harry",
+                        "family": "Potter"
+                    },
+                    "locale": "en_UK",
+                    "time_zone": "Europe/London",
+                    "contact_methods": [
+                        { "method": "facebook", "address": "harrypottermovie" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Martin",
+                        "family": "Brody"
+                    },
+                    "locale": "en_US",
+                    "time_zone": "America/New_York",
+                    "contact_methods": [
+                        { "method": "email", "address": "Martin.Brody@fakemail.com" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Ellen",
+                        "family": "Ripley"
+                    },
+                    "locale": "en_US",
+                    "contact_methods": [
+                        { "method": "SMS", "address": "ellen-ripley" }
+                    ]
+                },
+                {
+                    "name": {
+                        "given": "Jason",
+                        "family": "Bourne"
+                    },
+                    "locale": "en_US",
+                    "contact_methods": [
+                        { "method": "email", "address": "Jason.Bourne@fakemail.com" }
+                    ]
+                }
+            ]);
+        }
+    }
+});
+//# sourceMappingURL=mock-people.js.map
