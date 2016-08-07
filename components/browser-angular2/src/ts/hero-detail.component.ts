c67ae59d064c1c19f73c96eac4ae38b4d7a5297d
Peter Snider <snider.peter@gmail.com>
2016-08-07 14:09:50 -0700

    copied angular2-tutorial to browser-angular2
    to use as starting point

--- components/browser-angular2/src/ts/hero-detail.component.ts
+++ components/browser-angular2/src/ts/hero-detail.component.ts
@@ -1,24 +1,29 @@
-import {Component} from 'angular2/core';
-import {Hero} from './hero';
+import { Component, Input, OnInit } from 'angular2/core';
+import { RouteParams } from 'angular2/router';
 
-
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-hero-detail',
-  template: `
-      <div *ngIf="hero">
-          <h2>{{hero.name}} details!</h2>
-          <div><label>id: </label>{{hero.id}}</div>
-          <div>
-              <label>name: </label>
-              <input [(ngModel)]="hero.name" placeholder="name"/>
-          </div>
-      </div>
-      `,
-  inputs: ['hero']
+  templateUrl: 'html/hero-detail.component.html',
+  styleUrls: ['html/hero-detail.component.css']
 })
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
 
+  constructor(
+    private _heroService: HeroService,
+    private _routeParams: RouteParams) {
+  }
 
-export class HeroDetailComponent {
-    public hero: Hero;
+  ngOnInit() {
+    let id = +this._routeParams.get('id');
+    this._heroService.getHero(id)
+      .then((hero: Hero) => this.hero = hero);
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
--- components/browser-angular2/src/ts/hero.service.ts
+++ components/browser-angular2/src/ts/hero.service.ts
@@ -1,18 +1,23 @@
-import {Injectable} from 'angular2/core';
-import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
+import { Injectable } from 'angular2/core';
 
-
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-    getHeroes() {
-        return Promise.resolve(HEROES);
-    }
-    // See the "Take it slow" appendix
-    getHeroesSlowly() {
-      return new Promise<Hero[]>(resolve =>
-        setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
-      );
-    }
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+
+  getHeroesSlowly() {
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+    );
+  }
+
+  getHero(id: number) {
+    return Promise.resolve(HEROES).then(
+      (heroes: Hero[]) => heroes.filter(hero => hero.id === id)[0]
+    );
+  }
 }
