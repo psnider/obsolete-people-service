31e74fac75bd5a37b81a5d68cf1e0ce4754ad1fa
Peter Snider <snider.peter@gmail.com>
2016-08-06 18:52:36 -0700

    gulp test-browser-angular1 works

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,1 +1,4 @@
+/// <reference path="globals/angular-mocks/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
 /// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
