701a899b462eb5e6d3c8d27b13002f300a3902b1
Peter Snider <snider.peter@gmail.com>
2016-08-17 17:20:23 -0700

    reverted spec test

--- components/browser-angular2/ts/app.component.spec.ts
+++ components/browser-angular2/ts/app.component.spec.ts
@@ -1,83 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { Person } from './person';
-import { PersonDetailComponent } from './person-detail.component';
-import { PeopleService } from './people.service';
-@Component({
-  selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <h2>My People</h2>
-    <ul class="people">
-      <li *ngFor="let person of people"
-        [class.selected]="person === selectedPerson"
-        (click)="onSelect(person)">
-        <span class="badge">{{person.id}}</span> {{person.name}}
-      </li>
-    </ul>
-    <my-person-detail [person]="selectedPerson"></my-person-detail>
-  `,
-  styles: [`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .people {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 15em;
-    }
-    .people li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .people li.selected:hover {
-      background-color: #BBD8DC !important;
-      color: white;
-    }
-    .people li:hover {
-      color: #607D8B;
-      background-color: #DDD;
-      left: .1em;
-    }
-    .people .text {
-      position: relative;
-      top: -3px;
-    }
-    .people .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0 0 4px;
-    }
-  `],
-  directives: [PersonDetailComponent],
-  providers: [PeopleService]
-})
-export class AppComponent implements OnInit {
-  title = 'people-service title';
-  people: Person[];
-  selectedPerson: Person;
-  constructor(private peopleService: PeopleService) { }
-  getPeople() {
-    this.peopleService.getPeople().then(people => this.people = people);
-  }
-  ngOnInit() {
-    this.getPeople();
-  }
-  onSelect(person: Person) { this.selectedPerson = person; }
-}
+/* tslint:disable:no-unused-variable */
+import { AppComponent } from './app.component';
+
+import { async, inject } from '@angular/core/testing';
+
+import { TestComponentBuilder } from '@angular/core/testing';
+
+import { By }             from '@angular/platform-browser';
+import { provide }        from '@angular/core';
+import { ViewMetadata }   from '@angular/core';
+import { PromiseWrapper } from '@angular/core/src/facade/promise';
+
+////////  SPECS  /////////////
+
+/// Delete this
+describe('Smoke test', () => {
+  it('should run a passing test', () => {
+    expect(true).toEqual(true, 'should pass');
+  });
+});
+
+describe('AppComponent with TCB', function () {
+
+  it('should instantiate component',
+    async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+
+    tcb.createAsync(AppComponent).then(fixture => {
+      expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+    });
+  })));
+
+  it('should have expected <h1> text',
+    async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+
+      tcb.createAsync(AppComponent).then(fixture => {
+      // fixture.detectChanges();  // would need to resolve a binding but we don't have a binding
+
+      let h1 = fixture.debugElement.query(el => el.name === 'h1').nativeElement;  // it works
+
+          h1 = fixture.debugElement.query(By.css('h1')).nativeElement;            // preferred
+
+      expect(h1.innerText).toMatch(/people-service/i, '<h1> should say something about "people-service"');
+    });
+
+  })));
+});
